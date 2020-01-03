0897fae6e527ca1927efe86cf5c7d6b222cf4c47
maxjonasbehrens <47329186+maxjonasbehrens@users.noreply.github.com>
2020-01-03 13:22:10 +0100

    feature to subselect raw images

--- NeuralNetTrain/imgProcessing/sat_images.py
+++ NeuralNetTrain/imgProcessing/sat_images.py
@@ -2,8 +2,11 @@
 import numpy as np
 import pyrsgis
 from skimage.transform import resize
+from matplotlib import pyplot as plt
+import pandas as pd
 
-  # Function to create input data for CNN
+#%%
+# Function to create input data for CNN
 def create_data(files,path,y_dat, prediction,replace_nan = "mean",resolution = 256,night=True):
     x = []
     y = []
@@ -45,3 +48,35 @@ def create_y_label(files,path,y_dat,prediction):
         y.append(y_dat.loc[(y_dat['nuts2']==split1) & (y_dat['year']==split2),prediction])
         label.append(f)
     return y, label
+
+#%%
+# Create sampled image data based on raw data
+def create_sample_data(files,path,y_dat,prediction='nuts_value',resolution=256):
+    x_ = []
+    y_ = []
+    label = []
+    i = 0
+    for f in files:
+        if i % 10 == 0:
+            print("Image processed: ",str(i)," of ",str(len(files)))
+        ds, temp = pyrsgis.raster.read(str(path+f))
+        temp = np.moveaxis(temp,0,-1)
+
+        for k in range(51):
+            x = round(np.random.uniform(high = temp.shape[0]))
+            y = round(np.random.uniform(high = temp.shape[1]))
+            sample = temp[x:x+resolution,y:y+resolution,:]
+            while np.isnan(sample).any():
+                x = round(np.random.uniform(high = temp.shape[0]))
+                y = round(np.random.uniform(high = temp.shape[1]))
+                sample = temp[x:x+resolution,y:y+resolution,:]
+                # Maybe replace already taken values with nan to prevent overlapping
+            
+            x_.append(sample)
+            split1 = f.rsplit('_',1)[0]
+            split2 = int(f.rsplit('_',1)[1].rsplit('.',1)[0])
+            y_.append(y_dat.loc[(y_dat['nuts2']==split1) & (y_dat['year']==split2),prediction])
+            label.append(f)
+        
+        i += 1
+    return x_, y_, label
